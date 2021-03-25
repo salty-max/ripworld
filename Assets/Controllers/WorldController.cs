a2646cf652c87ff909a9e565d273c5fee08e5c51
max-jellycat <max@jellycat.fr>
2021-03-25 19:49:58 +0100

    Initial commit :wave:

---
+++ Assets/Controllers/WorldController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+  World world;
+  void Start()
+  {
+    world = new World();
+  }
+
+  void Update()
+  {
+
+  }
+}
